94cccc7a6f28b1305b5f71453f133552dddac470
ZAsabiha <zannatulsabiha@iut-dhaka.edu>
2025-05-25 19:18:18 +0600

    set-up

---
+++ hr-core/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     app 
+    </>
+  )
+}
+
+export default App
