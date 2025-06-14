dc254ff680b8ce23a3c10cf52fa3113071d21630
IRIvey <isrativey@iut-dhaka.edu>
2025-06-14 17:23:46 +0600

    Multiple Files Modified

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -22,7 +22,7 @@ const LoginForm = () => {
     setError('');
 
     try {
-      const response = await fetch('/api/login', {
+        const response = await fetch('http://localhost:5000/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
