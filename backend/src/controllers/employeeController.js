8c41b25aaf38fae45f96f349fd3b7a3e9430f174
Zannatul Adon Sabiha <166790186+ZAsabiha@users.noreply.github.com>
2025-07-12 12:13:34 +0600

    updated employeeController.js

--- backend/src/controllers/employeeController.js
+++ backend/src/controllers/employeeController.js
@@ -1,4 +1,5 @@
-import prisma from '../../prisma/client.js';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
 export const getEmployees = async (req, res) => {
   try {
@@ -11,4 +12,26 @@ export const getEmployees = async (req, res) => {
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch employees' });
   }
+}
+export const getEmployeeById = async (req, res) => {
+  const { id } = req.params;
+  console.log("➡️ Requested ID:", id);
+  try {
+    const employee = await prisma.employee.findUnique({
+      where: { id: Number(id) },
+      include: { department: true }
+    });
+    console.log("➡️ Prisma result:", employee);
+
+    if (!employee) {
+      return res.status(404).json({ error: 'Employee not found' });
+    }
+
+    res.json(employee);
+  } catch (err) {
+    console.error("➡️ Error fetching employee:", err);
+    res.status(500).json({ error: 'Failed to fetch employee' });
+  }
 };
+
+
