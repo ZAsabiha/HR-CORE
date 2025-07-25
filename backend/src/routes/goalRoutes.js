ce5f40eec35132df82cbf0f3a442df0731e02ee3
Zannatul Adon Sabiha <166790186+ZAsabiha@users.noreply.github.com>
2025-07-25 22:01:31 +0600

    added post req for adding goals

--- backend/src/routes/goalRoutes.js
+++ backend/src/routes/goalRoutes.js
@@ -1,6 +1,8 @@
 import express from 'express';
-import { getEmployeeGoals } from '../controllers/goalController.js';
+import { getEmployeeGoals, createGoal } from '../controllers/goalController.js';
 
 const router = express.Router();
 router.get('/', getEmployeeGoals);
+router.post('/', createGoal); 
 export default router;
+
