5c4eb537f2681cd553b0e6f8ec10ab4f79d23d6d
Zannatul Adon Sabiha <166790186+ZAsabiha@users.noreply.github.com>
2025-08-04 11:41:55 +0600

    Update employeeRoutes.js

--- backend/src/routes/employeeRoutes.js
+++ backend/src/routes/employeeRoutes.js
@@ -4,15 +4,19 @@ import {
   getEmployees,
   getEmployeeById,
   deleteEmployee,
-  updateEmployee
+  updateEmployee,
+  searchEmployees, 
 } from '../controllers/employeeController.js';
 
 const router = express.Router();
 
+
+router.get('/search', searchEmployees); 
+
 router.post('/', createEmployee);
 router.get('/', getEmployees);
 router.get('/:id', getEmployeeById);
-router.put('/:id', updateEmployee);  
+router.put('/:id', updateEmployee);
 router.delete('/:id', deleteEmployee);
 
 export default router;
