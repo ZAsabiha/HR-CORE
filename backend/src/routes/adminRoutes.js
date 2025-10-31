989f2565822f85381690f79f3b8829be8e8bbc02
Zannatul Adon Sabiha <166790186+ZAsabiha@users.noreply.github.com>
2025-10-31 19:36:05 +0600

    Delete backend/src directory

--- backend/src/routes/adminRoutes.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { getAdminProfile } from '../controllers/adminController.js';
-
-const router = express.Router();
-router.get('/profile', getAdminProfile);
-export default router;
